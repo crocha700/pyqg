c0147800ce5a41b94e11f72efa142624b353646d
Cesar B Rocha <crocha@ucsd.edu>
2015-10-24 20:10:51 -0700

    Rebasing with master

--- pyqg/model.py
+++ pyqg/model.py
@@ -3,8 +3,7 @@ import scipy as sp
 from kernel import PseudoSpectralKernel, tendency_forward_euler, tendency_ab2, tendency_ab3
 from numpy import pi
 import logging
-
-try:
+try:   
     import mkl
     np.use_fastnumpy = True
 except ImportError:
